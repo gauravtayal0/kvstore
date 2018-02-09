4464aa900a9faf91ec57df21d34b4a9eceab940d
gaurav <gaurav@moonfroglabs.com>
2018-02-09 16:33:38 +0530

    basic set, get and delete func added

---
+++ datastore.go
@@ -0,0 +1,44 @@
+package kvm
+
+import (
+	"sync"
+)
+
+type KV struct {
+	Key   string
+	Value string
+}
+
+type DataStore struct {
+	m   map[string]KV
+	mux sync.RWMutex
+}
+
+func New() DataStore {
+	ds := DataStore{m: make(map[string]KV)}
+	return ds
+}
+
+func (ds *DataStore) Set(key, value string) {
+	ds.mux.Lock()
+	ds.m[key] = KV{key, value}
+	ds.mux.Unlock()
+}
+
+func (ds *DataStore) Get(key string) (KV, bool) {
+	ds.mux.RLock()
+	kvpair, ok := ds.m[key]
+	ds.mux.RUnlock()
+	return kvpair, ok
+}
+
+func (ds *DataStore) GetValue(key string) (string, bool) {
+	ds.mux.RLock()
+	kv, ok := ds.m[key]
+	ds.mux.RUnlock()
+	return kv.Value, ok
+}
+
+func (ds *DataStore) Delete(key string) {
+	delete(ds.m, key)
+}
